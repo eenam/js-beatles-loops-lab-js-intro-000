26854bce77ebbe6589735f10ed75081d45818093
Emma Nam <en2458@gmail.com>
2020-11-12 19:12:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,31 +6,22 @@ function theBeatlesPlay(musicians, instruments){
   return array;
 }
 
-
-var facts = [
-"He was the last Beatle to learn to drive",
-"He was never a vegetarian",
-"He was a choir boy and boy scout",
-"He hated the sound of his own voice"
-];
 function johnLennonFacts(facts) {
   var array = [];
-  var i = 0;
-  while (i < facts.length){
-  array.push(facts[i] + "!!!");
-  i++;
-}
+  let i = 0
+  while (i < facts.length) {
+    array.push(facts[i] + '!!!')
+    i += 1
+  }
   return array;
 }
 
 
-function iLoveTheBeatles(i) {
-  var array = [];
-  do {
-    i++;
-    array.push('I love the Beatles!')
-  } while (i < 15);{
-
-}
-return array;
+function iLoveTheBeatles(num) {
+ var array = [];
+ do {
+   array.push("I love the Beatles!");
+   num++;
+ } while (num < 15)
+ return array;
 }
